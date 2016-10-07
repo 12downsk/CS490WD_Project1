d4431686779bff7e94575742d67d8c0f017b0c76
12downsk <12downks@gmail.com>
2016-10-07 10:50:34 -0500

    Dynamically Load Movie Content Two style sheets created, one for each view mode TODO: search feature, styling

--- public_html/functions.js
+++ public_html/functions.js
@@ -1,29 +1,47 @@
 $(document).ready(function(){
-   $("#movieListContainer").html(generatePage(0,0));
+   $("#movieListContainer").html(generatePage(movies,0,0)); //Initialize with all movies shown, in the grid view, sorted by year
+   $("#listView").on("click",listview);
+   $("#gridView").on("click",gridview);
 });
+
+function listview(){
+    $("#listView").addClass("active");
+    $("#gridView").removeClass("active");
+    $("#viewModeSheet").attr("href","listView.css");
+}
+
+function gridview(){
+    $("#gridView").addClass("active");
+    $("#listView").removeClass("active");
+    $("#viewModeSheet").attr("href","gridView.css");
+    
+}
 
 function search(input){
     
 }
 
-function generatePage(sorting,viewMode){
+function generatePage(data,sorting,viewMode){
     //sort the data, then iterate through it generating a chunk of html
     //for each movie
     if(sorting==0){//by year
-        movies.sort(function(a,b){return a.year - b.year;});
-
+        data["movies"].sort(function(a,b){return a.year - b.year;});
     }
     else{//by rating
-        movies.sort(function(a,b){return a.rating - b.rating});
+        data.sort(function(a,b){return a.rating - b.rating});
     }
-    $.map(movies,function(index,movie){
-        template(movie.title,movie.year,movie.starring,movie.description,movie.HD,movie.photo,movie.rating,sorting,viewMode);
+    var htmlString = "";
+    $.map(data["movies"],function(movie,index){
+        
+        htmlString += template(movie.title,movie.year,movie.starring,movie.description,movie.HD,movie.photo,movie.rating,viewMode);
     });
+    
+    $("#movieListContainer").html(htmlString);
 }
 
-function template(title,year,starring,description,HD,photo,rating,sorting,viewMode){
-    var HDimgPath;
-    var ratingCode;
+function template(title,year,starring,description,HD,photo,rating,viewMode){
+    var HDimgPath = "";
+    var ratingCode = "";
     if(HD){
         HDimgPath = "<img src=\"img/HD.png\" class=\"hd\">";
     }
@@ -36,18 +54,17 @@ function template(title,year,starring,description,HD,photo,rating,sorting,viewMo
     for(i=0;i<(5-rating);i++){
         ratingCode+="<img class=\"star\" src=\"img/regular_star.png\">";
     }
-    
-    $("#movieListContainer").append(
-        "<div class=\"movieItem\">"+
-            "<div class=\"movieFrame\">"+
-                "<img src=\""+photo+"\">"+HDimgPath+
+
+    return "<div class=\"movieItem\">"+
+            "<div>"+
+                "<img class=\"movieFrame\" src=\""+photo+"\">"+HDimgPath+
             "</div>"+
             "<div class=\"info\">"+
                 "<p class=\"title\">"+title+"<span class=\"year\">("+year+"</span></p>"+
                 "<p><b>Starring: </b>"+ starring +"</p>"+
                 "<p><b>Rating</b>"+ratingCode+"</p>"+
-                "<p>"+description+"</p>"+
+                "<p class=\"desc\">"+description+"</p>"+
             "</div>"+
         "</div>"
-    );
+
 }
